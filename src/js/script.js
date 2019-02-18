0373726aaa1369cbee3da9f640cfb52ea6b3f468
Dima <dnarizhnyi94@gmail.com>
2019-02-18 21:51:19 +0200

    с поправочкой

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,17 @@
-
 // Создаем переменные
 window.addEventListener('DOMContentLoaded', () => {
     const cartWrapper = document.querySelector('.cart__wrapper'),
-    cart = document.querySelector('.cart'),
-    close = document.querySelector('.cart__close'),
-    open = document.querySelector ('#cart'),
-    goodsBtn = document.querySelectorAll ('.goods__btn'),
-    products = document.querySelectorAll ('.goods__item'),
-    confirm = document.querySelector('.confirm'),
-    badge = document.querySelector('.nav_badge'),
-    totalost = document.querySelector('.cart__total > span'),
-    titles = document.querySelectorAll('.goods_title');
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.cart__close'),
+        open = document.querySelector('#cart'),
+        goodsBtn = document.querySelectorAll('.goods__btn'),
+        products = document.querySelectorAll('.goods__item'),
+        confirm = document.querySelector('.confirm'),
+        badge = document.querySelector('.nav_badge'),
+        totalost = document.querySelector('.cart__total > span'),
+        titles = document.querySelectorAll('.goods_title');
 
-//Описываем функцию открытия кнопки "корзина"
+    //Описываем функцию открытия кнопки "корзина"
     function openCart() {
         cart.style.display = 'block';
         document.body.style.overflow = 'hidden';
@@ -21,12 +20,12 @@ window.addEventListener('DOMContentLoaded', () => {
     function closeCart() {
         cart.style.display = 'none';
         document.body.style.overflow = '';
-    } 
-// обрабатываем событие касания и вызываем функцию закрытия-открытия
+    }
+    // обрабатываем событие касания и вызываем функцию закрытия-открытия
     open.addEventListener('click', openCart); // нажатие на кнопку корзины
     close.addEventListener('click', closeCart);
 
-// Разобрать этот код завтра =)
+    // Разобрать этот код завтра =)
     goodsBtn.forEach(function (btn, i) {
         btn.addEventListener('click', () => {
             let item = products[i].cloneNode(true),
@@ -46,6 +45,4 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-}); //посмотреть аттрибуты 
-
-
+}); //посмотреть аттрибуты
